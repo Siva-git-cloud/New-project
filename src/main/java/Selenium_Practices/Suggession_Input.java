4e56c356bf807a8f8e89a00493f7df613c271f8e
siva.k <sivakumar915443@gmail.com>
2025-07-03 22:07:01 +0530

    Xpath testing

---
+++ src/main/java/Selenium_Practices/Suggession_Input.java
@@ -0,0 +1,32 @@
+package Selenium_Practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+//Suggession Class Example--> Used URL -->https://rahulshettyacademy.com/AutomationPractice/
+// We need to enter the values in Suggession Class Example box it has to eneter the data in that need to display the values
+public class Suggession_Input {
+    public static void main(String[] args){
+        WebDriver driver=new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        WebElement enterSuggestion = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='autocomplete']")));
+        enterSuggestion.sendKeys("India");
+        List<WebElement> values = driver.findElements(By.xpath("//li[@class='ui-menu-item']//div"));
+        for (WebElement options :values){
+            System.out.println(options.getText());
+            if (options.getText().equalsIgnoreCase("India")){
+                options.click();
+                driver.quit();
+            }
+        }
+    }
+}
