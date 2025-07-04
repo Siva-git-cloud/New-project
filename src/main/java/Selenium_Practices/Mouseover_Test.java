9fd64e38591d0f9cafd31b23a123bd0c8a2564d8
siva.k <sivakumar915443@gmail.com>
2025-07-04 07:26:54 +0530

    Xpath testing

--- src/main/java/Selenium_Practices/Mouseover_Test.java
+++ src/main/java/Selenium_Practices/Mouseover_Test.java
@@ -1,7 +1,28 @@
 package Selenium_Practices;
 
-public class Mouseover_Test {
-    public static void main(String[] args){
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
+public class Mouseover_Test {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        Actions actions=new Actions(driver);
+        driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        WebElement button = driver.findElement(By.id("mousehover"));
+        actions.moveToElement(button).perform();
+        WebElement topOptions =wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Top")));
+        //WebElement reloadOptions= wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Reload")));
+        topOptions.click();
+        //reloadOptions.click();
+        //driver.quit();
     }
 }
