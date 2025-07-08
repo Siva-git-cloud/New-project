5f5ab96a202dd50683a7c2a87842f9ec058b82af
siva.k <sivakumar915443@gmail.com>
2025-07-08 11:09:22 +0530

    Xpath testing

---
+++ src/main/java/Selenium_Practices/Assignment_9.java
@@ -0,0 +1,37 @@
+package Selenium_Practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//info :: This class is used for the aleart pop-ups
+public class Assignment_9 {
+    public static void main(String[] args){
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement enterName = driver.findElement(By.id("name"));
+        enterName.sendKeys("Siva");
+        //For alraet key
+
+        //WebElement alreatButton= driver.findElement(By.id("alertbtn"));
+        //alreatButton.click();
+        //System.out.println(driver.switchTo().alert().getText());
+        //driver.switchTo().alert().accept();
+
+        //for confirm key for accepet
+        /*WebElement confirmButton=driver.findElement(By.id("confirmbtn"));
+        confirmButton.click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();*/
+
+        //for confirm key for cancel
+        WebElement confirmButton = driver.findElement(By.id("confirmbtn"));
+        confirmButton.click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+
+        //driver.quit();
+    }
+}
